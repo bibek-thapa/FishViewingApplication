4a41be39cbd8dd266485c763de20d6c3a784fb9b
bibek-thapa <bibek.thapahere1@gmail.com>
2019-04-25 12:26:12 -0400

    before the test

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,5 +1,23 @@
 class Fish < ApplicationRecord
 	has_many_attached :files
 	scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
+	validates:genus,presence:true,length:{minimum:5,maximum:20}
+	validates:description,presence:true,length:{minimum:5,maximum:120}
+	validates:commonname,presence:true,length:{minimum:5,maximum:20}
+	validate:image_type
+
+	private 
+	def image_type
+		if files.attached? == false
+			errors.add(:files,"are missing")
+
+		end
+
+		files.each do |file|
+				if !file.content_type.in?(%('image/jpeg image/png'))
+					errors.add(:files,"should be a jpeg or png")
+				end	
+		end
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates:email,presence:true,uniqueness:true
+  validates:email,presence:true,uniqueness:true,length:{minimum:5,maximum:20}
+  validates:firstname,presence:true,length:{minimum:2,maximum:20}
+  validates:lastname,presence:true,length:{minimum:2,maximum:80}
 end
